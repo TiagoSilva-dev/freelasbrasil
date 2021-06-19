105b0ec63adcfa635a41bac6aef71a5175dbb4ee
Tiago Silva <75397880+TiagoSilva-dev@users.noreply.github.com>
2021-06-19 15:09:20 -0300

    Update index.js

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './dashboard.css';
 import Header from '../../components/Header';
 import api from '../../services/api';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from '@material-ui/core/Button';
+
+
 export default function Dashboard() {
   const [freelas, setFreelas] = useState([]);
 
@@ -27,7 +31,7 @@ export default function Dashboard() {
           return (
             <article className="artigo">
               <header className="titulo">
-                <a href={item.link} target="blank">{item.titulo}</a>
+                <a href={item.link} target="blank">{item.titulo.toUpperCase()}</a>
               </header><span>{item.dias}</span>
               <section>
                 <div className="valor">
@@ -39,9 +43,14 @@ export default function Dashboard() {
                   </span>
                 </div>
               </section>
+              <div className="btn">
+                  <Button variant="contained" color="info.main" href={item.link} target="blank">
+                    Verificar Oportunidade
+                  </Button>
+                </div>
             </article>
           )
-        })) : (<span>Carregando ... </span>)}
+        })) : (<span>Carregando <CircularProgress /> </span>)}
 
       </main>
 
