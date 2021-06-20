c842bc3ba3fced9a0ae585394dfaada283fec1cf
Tiago Silva <tiagosilva.dev2020@gmail.com>
2021-06-20 13:30:50 -0300

    atualizando rotas

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from '../Pages/Home';
 import Dashboard from '../Pages/Dashboard';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/" component={Dashboard} />
       </Switch>
     </BrowserRouter>
 
